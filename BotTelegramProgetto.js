99ca6ca5d95f7a37cda14c11842764e99e7da549
simomerlo <56151826+simomerlo@users.noreply.github.com>
2020-05-02 18:25:09 +0200

    Update BotTelegramProgetto.js

--- BotTelegramProgetto.js
+++ BotTelegramProgetto.js
@@ -13,21 +13,101 @@ const bot = new TelegramBot(Token,
     });
 
 
-/*bot.on('message', (msg) =>
-{
-    var Hi="ciao";
-    if(msg.text.toString().toLowerCase().indexOf(Hi)===0)
-    {
-        bot.sendMessage(msg.chat.id, "Ciao mondo!");
-    }
-})*/
+
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome");
+    bot.sendMessage(msg.chat.id, "Welcome to FootballBot");
 });
 
-bot.onText(/\/match/, (msg, match) => {
 
+bot.onText(/\/giocatori/, (msg, team) => {
+    var giocatori = [];
+    var squadra = team.input.split(" ")[1];
+    bot.sendMessage(msg.chat.id, "Elenco giocatori:");
+    axios.get(uri + '?action=' + 'get_teams&league_id=148&APIkey=' + api)
+        .then(function (response) {
+            bot.sendMessage(msg.chat.id, "Elenco giocatori:");
+            if (response.length != 0) {
+                for (var i = 0; i < response.data.length; i++) {
+                    if (response.data[i].team_name == squadra) {
+                        for (var j = 0; j < response.data[i].players.length; j++) {
+                            var giocatore = "Nome: " + response.data[i].players[j].player_name + "\nNumero: " + response.data[i].players[j].player_number + "\nNazione: " + response.data[i].players[j].player_country + "\nRuolo: " + response.data[i].players[j].player_type + "\nEtà: " + response.data[i].players[j].player_age + "\n\n";
+                            giocatori.push(giocatore);
+                        }
+                    }
+
+                    bot.sendMessage(msg.chat.id, giocatori.join("\n"));
+                }
+
+            }
+
+        })
+});
+
+bot.onText(/\/allenatori/, (msg, team) => {
+
+    var allenatori = [];
+    var squadra = team.input.split(" ")[1];
+    bot.sendMessage(msg.chat.id, "Elenco allenatori:");
+    axios.get(uri + '?action=' + 'get_teams&league_id=148&APIkey=' + api)
+        .then(function (response) {
+
+            if (response.length != 0) {
+                for (var i = 0; i < response.data.length; i++) {
+                    if (response.data[i].team_name == squadra) {
+
+                        for (var j = 0; j < response.data[i].coaches.length; j++) {
+                            var allenatore = "Nome: " + response.data[i].coaches[j].coach_name + "\nNazione: " + response.data[i].coaches[j].coach_country + "\nEtà: " + response.data[i].coaches[j].coach_age + "\n\n";
+                            allenatori.push(allenatore);
+                        }
+                    }
+                    bot.sendMessage(msg.chat.id, allenatori.join("\n"));
+                }
+
+            }
+
+        })
+});
+
+
+bot.onText(/\/event/, (msg, event) => {
+
+
+    // var eventodiinizio= event.input.split()[];
+    // var eventodifine = event.input.split()[];
+    axios.get(uri + '?action=' + 'get_events&from=2019-04-01' + 'to=' + '&APIkey=' + api)
+        .then(function (response) {
+            // handle success
+            if (response.length != 0) {
+                console.log(response);
+                bot.sendMessage(msg.chat.id, response.data.country_name);
+            }
+
+        })
+});
+
+
+
+bot.onText(/\/probabilita/, (msg, probabilita) => {
+
+
+    // var eventodiinizio= event.input.split()[];
+    // var eventodifine = event.input.split()[];
+    axios.get(uri + '?action=' + 'get_odds&from=2019-04-02&to=2019-04-03' + '&APIkey=' + api)
+        .then(function (response) {
+            // handle success
+            if (response.length != 0) {
+                console.log(response);
+                // bot.sendMessage(msg.chat.id, response.data);
+            }
+
+        })
+});
+
+
+
+
+bot.onText(/\/match/, (msg, match) => {
 
     axios.get(uri + '?action=' + 'get_countries' + '&APIkey=' + api)
         .then(function (response) {
@@ -48,11 +128,10 @@ bot.onText(/\/player/, (msg, match) => {
             // handle success
             if (response.length != 0) {
                 console.log(response.data);
-                for(var i=0; i<response.data.length; i++)
-                {
-                    bot.sendMessage(msg.chat.id, "nome: "+ response.data[i].player_name + "\n" + response.data[i].player_number);    
+                for (var i = 0; i < response.data.length; i++) {
+                    bot.sendMessage(msg.chat.id, "nome: " + response.data[i].player_name + "\n" + response.data[i].player_number);
                 }
-                
+
             }
 
         })
@@ -63,4 +142,3 @@ bot.on("polling_error", function (err) {
     console.log(err);
 });
 
-//test
